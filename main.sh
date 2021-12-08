05bac121319d4a45bd90a55a17bded5624c99e0a
nicolasblancom <nicolasblancom@gmail.com>
2021-12-08 06:42:34 +0100

    Main entry point: menu to ask what task to execute

--- main.sh
+++ main.sh
@@ -41,15 +41,31 @@ function getVarFromFile() {
     echo $(cat $filePath);
 }
 
+function listIncludesItem {
+    local list="$1"
+    local item="$2"
+
+    if [[ $list =~ (^|[[:space:]])"$item"($|[[:space:]]) ]] ; then
+        # yes, list include item
+        return 0
+    else
+        return 1
+    fi
+
+    ## usage 
+    # if `listIncludesItem "$tasks" "$task"` ; then
+}
+
 ###################################
 
 ## variables to source
 temp_vars_dir="temp_vars";
 magento_version_temp_file_path="$temp_vars_dir/magento_version_temp_file";
 
-function createMagentoVersionTempFile() {
+## functions declarations
+function createMagentoVersionTempFileIfNotExists() {
     if [ ! -f $magento_version_temp_file_path ]; then
-        log "Elige versión de magento:";
+        local PS3="Elige versión de magento:";
 
         versiones="2.4.1 2.4.2";
 
@@ -57,24 +73,43 @@ function createMagentoVersionTempFile() {
         do
             if [ $version = "2.4.1" ]; then
                 log "2.4.1";
+                echo 
             elif [ $version = "2.4.2" ]; then
                 log "2.4.2";
+                echo
             else
                 exit;
             fi
             
             touch $magento_version_temp_file_path;
             echo $version > "$magento_version_temp_file_path";
+            return
+        done
+    fi
+}
+
+function askToSelectTask() {
+    if [ -f $magento_version_temp_file_path ]; then
+        local IFS='|'
+        local PS3="Elige script a ejecutar:"
+
+        tasks="Preparar local para una v. de magento | Crear nuevo proyecto local "
+
+        select task in $tasks
+        do
+            if [ $REPLY = 1 ]; then
+                log opc 1
+                echo
+            else
+                log otra
+                echo
+            fi
             exit;
         done
     fi
 }
-createMagentoVersionTempFile
 
+## code execution
+createMagentoVersionTempFileIfNotExists
 
-
-
-# log "Ejemplo de Menu de Opciones."
-# logInfo "Opción Seleccionada 1 !";
-# logError "Finaliza la Ejecución. !";
-# logWarn "Seleccione una Opción de 1 a 5.";
+askToSelectTask
